0fc46313d5c46018203dde628b99f18ad90f0eca
Mario Alberto Barragán Espinosa <>
2021-02-21 14:11:30 -0600

    Add test code missing StoryBoardViewController id

--- TestViewControllersTests/StoryBoardViewControllerTests.swift
+++ TestViewControllersTests/StoryBoardViewControllerTests.swift
@@ -4,6 +4,7 @@ import XCTest
 final class StoryBoardViewControllerTests: XCTestCase {
 
     func test_load_storyboardViewController() {
-        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let sut = sb.instantiateViewController(identifier: String(describing: StoryBoardViewController.self))
     }
 }
