21fa13ecd11e170f45f460938c4b843d14afdc2a
Mario Alberto Barragán Espinosa <>
2021-02-28 16:08:03 -0600

    Create XIBViewController

---
+++ TestViewControllers/XIBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  XIBViewController.swift
+//  TestViewControllers
+//
+//  Created by Mario Alberto Barragán Espinosa on 28/02/21.
+//
+
+import UIKit
+
+class XIBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
