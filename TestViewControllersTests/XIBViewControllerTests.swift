9f529ca9fc4b80c103c3acd8df742454b56a1224
Mario Alberto Barragán Espinosa <>
2021-02-28 16:38:39 -0600

    Add loadViewIfNeeded

--- TestViewControllersTests/XIBViewControllerTests.swift
+++ TestViewControllersTests/XIBViewControllerTests.swift
@@ -6,6 +6,8 @@ final class XIBViewControllerTests: XCTestCase {
     func test_load_XIBViewController() {
         let sut = XIBViewController()
         
+        sut.loadViewIfNeeded()
+        
         XCTAssertEqual(sut.titleLabel.text, "Test")
     }
 }
